454e1d92c3c81a7d8e498a244e5e93fd3b0c1f17
Stefan Hagspiel <shagspiel@dachcom.ch>
2020-08-07 10:33:45 +0200

    fix detail url slugify listener

--- src/NewsBundle/EventListener/NewsSeoListener.php
+++ src/NewsBundle/EventListener/NewsSeoListener.php
@@ -2,12 +2,13 @@
 
 namespace NewsBundle\EventListener;
 
-use NewsBundle\Configuration\Configuration;
-use Pimcore\Cache;
-use Pimcore\Db;
+use Pimcore\Model\DataObject\Listing;
 use Pimcore\Event\Model\DataObjectEvent;
 use Pimcore\Event\DataObjectEvents;
-use Pimcore\Model\Version;
+use Pimcore\Model\DataObject\AbstractObject;
+use Pimcore\Model\DataObject\NewsCategory;
+use Pimcore\Model\DataObject\NewsEntry;
+use NewsBundle\Configuration\Configuration;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class NewsSeoListener implements EventSubscriberInterface
@@ -18,8 +19,6 @@ class NewsSeoListener implements EventSubscriberInterface
     protected $configuration;
 
     /**
-     * RestrictionServiceListener constructor.
-     *
      * @param Configuration $configuration
      */
     public function __construct(Configuration $configuration)
@@ -33,144 +32,111 @@ class NewsSeoListener implements EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return [
-            DataObjectEvents::PRE_ADD    => 'handleObjectSeo',
-            DataObjectEvents::PRE_UPDATE => 'handleObjectSeo'
+            DataObjectEvents::PRE_ADD    => 'handleObjectSeoAdd',
+            DataObjectEvents::PRE_UPDATE => 'handleObjectSeoUpdate'
         ];
     }
 
     /**
-     * @param DataObjectEvent $e
+     * @param DataObjectEvent $event
      */
-    public function handleObjectSeo(DataObjectEvent $e)
+    public function handleObjectSeoAdd(DataObjectEvent $event)
     {
-        $newsObject = $e->getObject();
-        $className = (new \ReflectionClass($newsObject))->getShortName();
-
-        if (!in_array($className, ['NewsEntry', 'NewsCategory'])) {
+        $object = $event->getObject();
+        if (!$object instanceof NewsEntry && !$object instanceof NewsCategory) {
             return;
         }
 
-        Version::disable();
-        $this->parseUrl($newsObject, $className);
-        Version::enable();
+        $languages = \Pimcore\Tool::getValidLanguages();
+        foreach ($languages as $language) {
+            //always reset stored url if element just has been copied.
+            $object->setDetailUrl(null, $language);
+        }
 
-        Cache::clearTag('object_' . $newsObject->getId());
+        // in case it's a copy!
+        $object->setPublished(false);
     }
 
     /**
-     * @param \Pimcore\Model\DataObject\AbstractObject $object
-     * @param string (NewsEntry|NewsCategory)      $className
-     *
-     * @return bool
-     * @throws \Exception
-     * @throws \Pimcore\Model\Element\ValidationException
+     * @param DataObjectEvent $event
      */
-    private function parseUrl($object, $className = '')
+    public function handleObjectSeoUpdate(DataObjectEvent $event)
+    {
+        $object = $event->getObject();
+        if (!$object instanceof NewsEntry && !$object instanceof NewsCategory) {
+            return;
+        }
+
+        $this->parseUrl($object);
+    }
+
+    /**
+     * @param AbstractObject $object
+     */
+    private function parseUrl($object)
     {
         $languages = \Pimcore\Tool::getValidLanguages();
 
-        $fromCopy = false;
-
-        $objectClass = 'Pimcore\\Model\\DataObject\\' . $className;
-        foreach ($languages as $language) {
-            if ($this->isFromCopy($object, $objectClass, $language) === true) {
-                $fromCopy = true;
-                break;
-            }
-        }
-
-        if ($fromCopy) {
-            foreach ($languages as $language) {
-                //always reset stored url if element just has been copied.
-                $object->setDetailUrl(null, $language);
-            }
-        }
+        /** @var AbstractObject $objectClass */
+        $objectClass = get_class($object);
+        $objectListingClass = sprintf('%s\Listing', $objectClass);
 
         $oldObject = null;
-        if ($object->getId()) {
-
-            //@todo: use versions?
-            $oldObject = \Pimcore::getContainer()->get('pimcore.model.factory')->build($objectClass);
-            $oldObject->getDao()->getById($object->getId());
+        if ($object->getId() > 0) {
+            $oldObject = $objectClass::getById($object->getId(), true);
         }
 
         foreach ($languages as $language) {
-            $realUrl = null;
-
-            $currentDetailUrl = $object->getDetailUrl($language);
-            $title = $object->getName($language);
-
-            //skip all empty!
-            if (empty($title) && empty($currentDetailUrl)) {
-                continue;
-            }
-
-            if (!empty($title)) {
-                $realUrl = $this->slugify(empty($currentDetailUrl) ? $title : $currentDetailUrl, $language);
-                if (empty($currentDetailUrl)) {
-                    $currentDetailUrl = $realUrl;
-                }
-            }
-
-            $versionUrl = $realUrl;
 
             $oldDetailUrl = null;
+
+            $title = $object->getName($language);
+            $currentDetailUrl = $object->getDetailUrl($language);
+
             if ($oldObject instanceof $objectClass) {
                 $oldDetailUrl = $oldObject->getDetailUrl($language);
             }
 
-            if ($oldDetailUrl !== $currentDetailUrl) {
-                $sameUrlObject = $objectClass::getByLocalizedfields(
-                    'detailUrl', $realUrl, $language,
-                    ['limit' => 1, 'condition' => ' AND ooo_id != ' . (int)$object->getId() . ' AND name <> ""']
-                );
-
-                if (is_array($sameUrlObject) && count($sameUrlObject) === 1) {
-                    $nextPossibleVersion = $sameUrlObject;
-
-                    $version = 1;
-                    $versionUrl = $realUrl . '-' . ($version);
-
-                    while ($nextPossibleVersion instanceof $objectClass) {
-                        $versionUrl = $realUrl . '-' . ($version);
-
-                        $nextPossibleVersion = $objectClass::getByLocalizedfields(
-                            'detailUrl', $versionUrl, $language,
-                            ['limit' => 1, 'condition' => ' AND ooo_id != ' . (int)$object->getId() . ' AND name <> ""']
-                        );
-
-                        $version++;
-                    }
-                }
-
-                $object->setDetailUrl($versionUrl, $language);
+            // title and detail url is empty, nothing we can do
+            if (empty($title) && empty($currentDetailUrl)) {
+                continue;
             }
+
+            // title is empty but detail url has been set: not allowed
+            if (empty($title) && !empty($currentDetailUrl)) {
+                $currentDetailUrl = null;
+                $object->setDetailUrl($currentDetailUrl, $language);
+                return;
+            }
+
+            // title is not empty but we have no detail url: build it!
+            if (!empty($title) && empty($currentDetailUrl)) {
+                $currentDetailUrl = $this->slugify($title, $language);
+            }
+
+            // current and last given detail url haven't changed: skip
+            if ($oldDetailUrl === $currentDetailUrl) {
+                continue;
+            }
+
+            // ensure current detail url has a valid slug
+            $currentDetailUrl = $this->slugify($currentDetailUrl, $language);
+
+            if ($this->otherElementsExists($objectListingClass, $language, $currentDetailUrl, $object->getId()) === false) {
+                $object->setDetailUrl($currentDetailUrl, $language);
+                continue;
+            }
+
+            $version = 1;
+            $originalUrl = $currentDetailUrl;
+            $nextLevelDetailUrl = sprintf('%s-%d', $originalUrl, $version);
+
+            while ($this->otherElementsExists($objectListingClass, $language, $nextLevelDetailUrl, $object->getId()) !== false) {
+                $nextLevelDetailUrl = sprintf('%s-%d', $originalUrl, ++$version);
+            }
+
+            $object->setDetailUrl($nextLevelDetailUrl, $language);
         }
-    }
-
-    /**
-     * @param $obj
-     * @param $objectClass
-     * @param $language
-     *
-     * @return bool
-     */
-    private function isFromCopy($obj, $objectClass, $language)
-    {
-        //always check if there is double data!!!
-        $name = $obj->getName($language);
-        $url = $obj->getDetailUrl($language);
-
-        if (empty($url)) {
-            return false;
-        }
-
-        $duplicateUrlObjects = $objectClass::getByLocalizedfields(
-            'detailUrl', $url, $language,
-            ['limit' => 1, 'condition' => ' AND name = "' . Db::get()->quote($name) . '" AND ooo_id != ' . (int)$obj->getId()]
-        );
-
-        return is_array($duplicateUrlObjects) && count($duplicateUrlObjects) === 1;
     }
 
     /**
@@ -179,7 +145,7 @@ class NewsSeoListener implements EventSubscriberInterface
      *
      * @return string
      */
-    private function slugify($string, $language)
+    protected function slugify($string, $language)
     {
         //remove dashes first
         $string = preg_replace('/[-\s]+/', ' ', $string);
@@ -189,11 +155,31 @@ class NewsSeoListener implements EventSubscriberInterface
         }
 
         $string = preg_replace(['/®/', '/©/'], '', $string);
-        $string = transliterator_transliterate("Any-Latin; Latin-ASCII; NFD; [:Nonspacing Mark:] Remove; NFC; [:Punctuation:] Remove; Lower();",
-            $string);
+        $string = transliterator_transliterate("Any-Latin; Latin-ASCII; NFD; [:Nonspacing Mark:] Remove; NFC; [:Punctuation:] Remove; Lower();", $string);
         // Remove repeating hyphens and spaces (e.g. 'foo---bar' becomes 'foo-bar')
         $string = preg_replace('/[-\s]+/', '-', $string);
 
         return trim($string, '-');
+    }
+
+    /**
+     * @param string $objectListingClass
+     * @param string $locale
+     * @param string $detailUrl
+     * @param int    $id
+     *
+     * @return bool
+     */
+    protected function otherElementsExists(string $objectListingClass, string $locale, string $detailUrl, int $id)
+    {
+        /** @var Listing $listing */
+        $listing = new $objectListingClass();
+        $listing->setUnpublished(true);
+        $listing->setLocale($locale);
+        $listing->setLimit(1);
+        $listing->addConditionParam('detailUrl = ?', $detailUrl);
+        $listing->addConditionParam('ooo_id != ?', $id);
+
+        return $listing->getCount() > 0;
     }
 }
